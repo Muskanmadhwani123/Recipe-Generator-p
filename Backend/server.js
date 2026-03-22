f7676507f893930a49dd80454a133fc3850cf968
muskanmadhwani123 <madhwanimuskan123@gmail.com>
2026-03-22 11:27:00 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -10,28 +10,81 @@ app.use(cors());
 
 const upload = multer({ dest: "uploads/" });
 
-app.post("/upload", upload.single("image"), async (req, res) => {
+console.log("File is running...");
 
+app.post("/upload", upload.single("image"), async (req, res) => {
 
     const form = new FormData();
     form.append("image", fs.createReadStream(req.file.path));
 
     try {
-
-        const response = await axios.post(
+        // 1. Get food prediction
+        const mlResponse = await axios.post(
             "http://127.0.0.1:5000/predict",
             form,
             { headers: form.getHeaders() }
         );
 
-        res.json({ food: response.data.prediction });
+        const foodName = mlResponse.data.prediction;
+        console.log("Detected food:", foodName);
+
+        // 2. Get recipe from Spoonacular
+        const recipeResponse = await axios.get(
+            "https://api.spoonacular.com/recipes/complexSearch",
+            {
+                params: {
+                    query: foodName,
+                    number: 1,
+                    addRecipeInformation: true,
+                    apiKey: "da16aa51c0154effa5d950830a7090d9"
+                }
+            }
+        );
+
+        const results = recipeResponse.data.results;
+
+        if (results.length === 0) {
+            return res.json({
+                food: foodName,
+                title: "No recipe found",
+                ingredients: [],
+                instructions: "Try another dish image."
+            });
+        }
+
+        const recipe = results[0] || {};
+        const recipeId = recipe.id;
+
+        const detailResponse = await axios.get(
+            `https://api.spoonacular.com/recipes/${recipeId}/information`,
+            {
+                params: {
+                    apiKey: "da16aa51c0154effa5d950830a7090d9"
+                }
+            }
+        );
+
+        const fullRecipe = detailResponse.data;
+
+       res.json({
+        food: foodName,
+        title: fullRecipe.title,
+        image: fullRecipe.image,
+        ingredients: fullRecipe.extendedIngredients
+            ? fullRecipe.extendedIngredients.map(i => i.original)
+            : ["No ingredients available"],
+        instructions: fullRecipe.instructions || "No instructions available"
+        });
+
 
     } catch (error) {
-        res.status(500).send("Error connecting to ML API");
+        console.error(error.message);
+        res.status(500).json({
+    error: "Error processing request"
+});
     }
-
 });
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+        console.log("Server running on port 3000");
+        });
